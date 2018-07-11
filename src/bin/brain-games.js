8de155fdf549c5b27f9a7418007759809bcfcbee
agrazh <andy.grazh@gmail.com>
2018-07-11 11:38:57 +0300

    fix

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
-import { getName } from '..';
+
+import readlineSync from 'readline-sync';
+// import { getName } from '..';
+
+const getName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+  return name;
+};
 
 console.log('Welcome to the Brain Games!\n');
 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import readlineSync from 'readline-sync';
 import { randomInt, pickRandom } from 'mathjs';
 
-export const getName = () => {
+const questionsMaxCount = 3;
+
+const getName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!\n`);
   return name;
 };
 
-// getName(); // если раскомментировать, то brain-games.js два раза запросит ввести имя :-/
+const isEven = number => number % 2 === 0;
 
 export const playEvenGame = () => {
-  const isEven = number => (number % 2 === 0 ? 'yes' : 'no');
-
   console.log('Welcome to Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".\n');
   const userName = getName();
-
-  const questionsMaxCount = 3;
 
   for (let i = 1; i <= questionsMaxCount; i += 1) {
     const question = randomInt(1, 99);
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
 
-    if (isEven(question) !== answer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(question)}'.\nLet's try again, ${userName}!`);
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
+
+    if (correctAnswer !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
       return;
     }
   }
@@ -50,8 +50,6 @@ export const playCalcGame = () => {
   console.log('What is the result of the expression?\n');
   const userName = getName();
 
-  const questionsMaxCount = 3;
-
   for (let i = 1; i <= questionsMaxCount; i += 1) {
     const question = [randomInt(1, 10), pickRandom(['+', '-', '*']), randomInt(1, 10)];
 
@@ -68,5 +66,3 @@ export const playCalcGame = () => {
 
   console.log(`Congratulations, ${userName}!`);
 };
-
-// playCalcGame();
