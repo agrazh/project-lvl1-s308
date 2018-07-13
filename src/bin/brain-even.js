94e644ac6ce822b319046c7ed6a48e8814c7a7f6
agrazh <andy.grazh@gmail.com>
2018-07-13 09:50:14 +0300

    improve brain-even game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import playEvenGame from '../games/even';
-// import { playEvenGame } from '../games/even'; - так не импортирует
 
 playEvenGame();
--- src/games/even.js
+++ src/games/even.js
@@ -4,22 +4,23 @@
 
 import { randomInt } from 'mathjs';
 import playGame from '..';
-// import { playGame } from '..'; - так не импортирует
 
 const gameTask = 'Answer "yes" if number even otherwise answer "no".';
 
 const isEven = number => number % 2 === 0;
 
 const genQuestionAndCorrectAnswer = () => {
-  const questionElements = randomInt(1, 99);
-  const question = (`Question: ${questionElements}`);
-  const correctAnswer = isEven(questionElements) ? 'yes' : 'no';
+  const number = randomInt(1, 99);
+  const question = (`Question: ${number}`);
 
-  return [question, correctAnswer];
+  switch (isEven(number)) {
+    case true:
+      return [question, 'yes'];
+    case false:
+      return [question, 'no'];
+    default:
+      return 'Unknown result of isEven() function';
+  }
 };
 
-const playEvenGame = () => {
-  playGame(gameTask, genQuestionAndCorrectAnswer);
-};
-
-export default playEvenGame;
+export default () => playGame(gameTask, genQuestionAndCorrectAnswer);
